8d6b3be086b824246b99437d4fd1b476793ed611
cyber-sergey <zenkovich_so@mail.ru>
2023-07-23 23:58:26 +0800

    Implementation telegram notification

--- src/main/java/apps/juice_up/config/BotInitializer.java
+++ src/main/java/apps/juice_up/config/BotInitializer.java
@@ -13,7 +13,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Slf4j
 @Component
-public class Initializer {
+public class BotInitializer {
     @Autowired
     TelegramNotificationBot bot;
 
